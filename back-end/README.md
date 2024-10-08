4169993b54a80c5603f738ee2464ac6a5b09366a
manisha sinha <bits.manisha@gmail.com>
2024-10-08 18:34:48 +0200

    login app added

---
+++ back-end/README.md
@@ -0,0 +1,10 @@
+# Backend for the Login app
+
+## Building and running
+
+To run the backend api:
+
+1. Install XAMPP to host php app and database
+2. Copy back-end folder and deploy it to htdocs folder C:\xampp\htdocs.
+3. Start Apache server, after which login api will be available on https://localhost/back-end/api
+4. Start MySQL DataBase
---
+++ back-end/api/user/login.php
@@ -0,0 +1,43 @@
+<?php
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/user.php';
+
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // allow all origins
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// prepare user object
+$user = new User($db);
+// set ID property of user to be edited
+$user->username = isset($_GET['email']) ? $_GET['email'] : die();
+$user->password = base64_encode(isset($_GET['password']) ? $_GET['password'] : die());
+// read the details of user to be edited
+$stmt = $user->login();
+if($stmt->rowCount() > 0){
+    // get retrieved row
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    // create array
+    $user_arr=array(
+        "status" => true,
+        "message" => "Successfully Login!",
+        "id" => $row['id'],
+        "email" => $row['email']
+    );
+}
+else{
+    $user_arr=array(
+        "status" => false,
+        "message" => "Invalid Email or Password!",
+    );
+}
+// make it json format
+print_r(json_encode($user_arr));
+?>
